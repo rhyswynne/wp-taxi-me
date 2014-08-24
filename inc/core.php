2f066ab5025856ab7d717be4fafacdc2fb7da333
Rhys Wynne <rhys@winwar.co.uk>
2014-08-24 20:49:29 +0100

    Added Widget functionality

--- inc/core.php
+++ inc/core.php
@@ -2,5 +2,6 @@
 
 require_once( WP_TAXI_ME_PLUGIN_PATH . '/inc/options.php' );
 require_once( WP_TAXI_ME_PLUGIN_PATH . '/inc/display-options.php' );
+require_once( WP_TAXI_ME_PLUGIN_PATH . '/inc/widget.php' );
 
 ?>
